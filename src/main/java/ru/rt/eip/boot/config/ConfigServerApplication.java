313aca5a5f78e4ab217529badb3dede02ea6ef45
Sergey Kovalenko <sergey.kovalenko@rt.ru>
2017-07-29 19:41:54 +0300

    Last update

--- src/main/java/ru/rt/eip/boot/config/ConfigServerApplication.java
+++ src/main/java/ru/rt/eip/boot/config/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package ru.rt.eip.boot.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
